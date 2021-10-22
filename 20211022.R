642b59a3ac68f5df392d63f8eed312a62d773d80
Woodruff <bmwoodruff@gmail.com>
2021-10-22 13:32:48 -0600

    class Oct 22

---
+++ 20211022.R
@@ -0,0 +1,21 @@
+f <- function(x){
+  sqrt(x^2+(8/x)^2)
+}
+
+x<- seq(2,3,0.1)
+plot(x, 8/x,xlim=c(-10,10),ylim = c(-10,10),type ='l')
+
+plot(x,f(x),type ='l')
+sqrt(8)
+
+
+x<- seq(2.828,2.8285,0.0001)
+plot(x,f(x),type ='l')
+
+derf <- function(x){
+  1/2*(x^2+64*x^(-2))^(-1/2)*(2*x-128*x^(-3))
+}
+uniroot(derf,c(2,3))$root
+
+
+
